52d02da246607ffa2a30e5e95c7be1c592125298
yejiaoti <112063593+yejiaoti@users.noreply.github.com>
2024-08-03 16:25:39 +0800

    Update ppt.js

--- music/ppt/ppt.js
+++ music/ppt/ppt.js
@@ -7,9 +7,9 @@
 				countTime += 1;
 				if(countTime % 10 == 7){
 					countImg += 1;
-					bg1Container.style = `background-image: url(http://yurun.ueuo.com/api/bdimg.php?img=美女&${countImg})`;
+					bg1Container.style = `background-image: url(http://yurun.ueuo.com/api/bdimg.php?img=meinv&${countImg})`;
 				}else if((countTime + 5) % 10 == 7){
 					countImg += 1;
-					bg2Container.style = `background-image: url(http://yurun.ueuo.com/api/bdimg.php?img=二次元&${countImg})`;
+					bg2Container.style = `background-image: url(http://yurun.ueuo.com/api/bdimg.php?img=erciyuan&${countImg})`;
 				}
 			}, 1000);
