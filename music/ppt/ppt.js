16e0f18e0fc814da28c9914450c67e70f5c42751
yejiaoti <112063593+yejiaoti@users.noreply.github.com>
2024-08-07 14:24:13 +0800

    Update ppt.js

--- music/ppt/ppt.js
+++ music/ppt/ppt.js
@@ -7,9 +7,9 @@
 				countTime += 1;
 				if(countTime % 10 == 7){
 					countImg += 1;
-					bg1Container.style = `background-image: url(http://yurun.ueuo.com/api/bdimg.php?img=meinv&${countImg})`;
+					bg1Container.style = `background-image: url(http://yurun.ueuo.com/tu/tcimg.php?img=meinv&${countImg})`;
 				}else if((countTime + 5) % 10 == 7){
 					countImg += 1;
-					bg2Container.style = `background-image: url(http://yurun.ueuo.com/api/bdimg.php?img=erciyuan&${countImg})`;
+					bg2Container.style = `background-image: url(http://yurun.ueuo.com/tu/tcimg.php?img=erciyuan&${countImg})`;
 				}
 			}, 1000);
